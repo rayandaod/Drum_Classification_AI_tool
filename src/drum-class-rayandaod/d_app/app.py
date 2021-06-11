1e959e8da7817a14a9ea1820da3876caa53587b9
Rayan Daod <rayan.daodnathoo@epfl.ch>
2021-06-11 10:53:42 +0200

    Add the empty datasets folder to git

--- src/drum-class-rayandaod/d_app/app.py
+++ src/drum-class-rayandaod/d_app/app.py
@@ -1,29 +1,57 @@
 import sys
 import os
 import math
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QListWidgetItem
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 from PyQt5.QtCore import Qt
 
 sys.path.append(os.path.abspath(os.path.join('')))
 
-from c_train.predict import predict
-from z_helpers.paths import *
 from config import *
-
-here = Path(__file__).parent
+from z_helpers.paths import *
+from c_train.predict import predict
 
 WIN_WIDTH = 1200
 WIN_HEIGHT = 600
 
+here = Path(__file__).parent
 style_sheet = open(here / APP_STYLE_SHEET)
+
+DATASET_FOLDER = '20210609-025547-My_samples'
+MODEL_FOLDER = 'RF_20210609-234422'
+
+
+def addSample(table, sample_path):
+    row = table.rowCount()
+    table.setRowCount(row + 1)
+    cell = QTableWidgetItem(sample_path)
+    cell.setFlags(Qt.ItemIsEnabled)
+    table.setItem(row, 0, cell)
+
+
+def fill_predictions(table, predictions_dict, unreadable_files, quiet_outliers):
+    for i, pred in predictions_dict.items():
+        cell = QTableWidgetItem(pred)
+        cell.setFlags(Qt.ItemIsEnabled)
+        table.setItem(int(i), 1, cell)
+
+    for i, pred in unreadable_files.items():
+        cell = QTableWidgetItem('Unreadable')
+        cell.setFlags(Qt.ItemIsEnabled)
+        table.setItem(int(i), 1, cell)
+
+    for i, pred in quiet_outliers.items():
+        cell = QTableWidgetItem('Too quiet')
+        cell.setFlags(Qt.ItemIsEnabled)
+        table.setItem(int(i), 1, cell)
 
 
 class TableWidget(QTableWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
         self.setAcceptDrops(True)
-        layout = QHBoxLayout()
-        self.setLayout(layout)
+        self.setColumnCount(2)
+        self.setHorizontalHeaderLabels(['Sample', 'Class'])
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls:
@@ -43,80 +71,62 @@ class TableWidget(QTableWidget):
             event.setDropAction(Qt.CopyAction)
             event.accept()
 
-            links = []
             for url in event.mimeData().urls():
-                # https://doc.qt.io/qt-5/qurl.html
                 if url.isLocalFile():
-                    print(url.path())
-                    for input_file in Path(url.path()).glob('**/*.wav'):
-                        absolute_path_name = input_file.resolve().as_posix()
-                        links.append(absolute_path_name.replace(GlobalConfig.SAMPLE_LIBRARY, ''))
-                else:
-                    links.append(str(url.toString()))
-            self.addItems(links)
+                    if os.path.isdir(url.path()):
+                        for input_file in Path(url.path()).glob('**/*.wav'):
+                            absolute_path_name = input_file.resolve().as_posix()
+                            sample_path = absolute_path_name.replace(GlobalConfig.SAMPLE_LIBRARY, '')
+                            addSample(self, sample_path)
+                    elif os.path.isfile(url.path()):
+                        absolute_path_name = Path(url.path()).resolve().as_posix()
+                        sample_path = absolute_path_name.replace(GlobalConfig.SAMPLE_LIBRARY, '')
+                        addSample(self, sample_path)
         else:
             event.ignore()
 
-
-class ToolBarWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.resize(WIN_WIDTH, 120)
-        layout = QVBoxLayout()
-
-        self.combo_datasets = QComboBox()
-        self.combo_datasets.addItems(next(os.walk(MODELS_PATH))[1])
-        self.combo_datasets.currentIndexChanged.connect(self.datasetSelectionChange)
-        layout.addWidget(self.combo_datasets)
-
-        self.combo_models = QComboBox()
-        self.combo_models.addItems(next(os.walk(MODELS_PATH / self.combo_datasets.currentText()))[1])
-        self.combo_models.currentIndexChanged.connect(self.modelSelectionChange)
-        layout.addWidget(self.combo_models)
-
-        # self.btn = QPushButton('Predict', self)
-        # self.btn.setGeometry(0, 545, 200, 50)
-        # self.btn.setStyleSheet(style_sheet.read())
-        # self.btn.clicked.connect(lambda: self.onPressButton())
-        # layout.addWidget(self.btn)
-
-    def datasetSelectionChange(self, i):
-        current_dataset = self.combo_datasets.currentText()
-        for i in range(self.combo_models.count()):
-            self.combo_models.removeItem(i)
-        self.combo_models.addItems(next(os.walk(MODELS_PATH / current_dataset))[1])
-
-    def modelSelectionChange(self, i):
-        return
-
-    def onPressButton(self):
-        item = QListWidgetItem(self.listbox_view.currentItem())
-        DATASET_FOLDER = self.combo_datasets.currentText()
-        MODEL_FOLDER = self.combo_models.currentText()
-        drum_types = predict(item.text(), DATASET_FOLDER, MODEL_FOLDER)
-        print(drum_types)
+    def getSamplePathDict(self):
+        sample_path_dict = dict()
+        for i in range(self.rowCount()):
+            sample_path_dict[str(i)] = Path(GlobalConfig.SAMPLE_LIBRARY + self.item(i, 0).text())
+        return sample_path_dict
 
 
-class DrumClassificationWindow(QMainWindow):
+class MainWidget(QWidget):
+
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Drum Classification')
         self.setFixedSize(WIN_WIDTH, WIN_HEIGHT)
 
-        self.table_view = TableWidget(self)
-        self.table_view.setGeometry(0, 0, WIN_WIDTH, int(WIN_HEIGHT * 4 / 5))
+        layout = QVBoxLayout()
 
-        self.toolbar_view = ToolBarWidget(self)
-        toolbar_height = math.floor(WIN_HEIGHT - self.table_view.height())
-        self.toolbar_view.setGeometry(0, WIN_HEIGHT - toolbar_height, WIN_WIDTH, toolbar_height)
-        self.toolbar_view.setStyleSheet('background: white')
+        table = TableWidget()
+        layout.addWidget(table)
+        table.setFixedSize(1160, 500)
+        table.setColumnWidth(0, math.ceil(table.width() / 4 * 3))
+        table.setColumnWidth(1, math.floor(table.width() / 4))
+
+        self.btn = QPushButton('Predict', self)
+        self.btn.clicked.connect(lambda: self.onPressButton(table))
+        layout.addWidget(self.btn)
+
+        self.setLayout(layout)
+        self.show()
+
+    def onPressButton(self, table):
+        sample_path_dict = table.getSamplePathDict()
+        # DATASET_FOLDER = self.combo_datasets.currentText()
+        # MODEL_FOLDER = self.combo_models.currentText()
+        try:
+            prediction_dict, unreadable_files, quiet_outliers = predict(sample_path_dict, DATASET_FOLDER, MODEL_FOLDER,
+                                                                        is_sample_dict=True)
+            fill_predictions(table, prediction_dict, unreadable_files, quiet_outliers)
+        except:
+            print("Unexpected error:", sys.exc_info())
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = DrumClassificationWindow()
-    window.show()
-
-    try:
-        sys.exit(app.exec_())
-    except SystemExit:
-        print('Closing Window...')
+    widget = MainWidget()
+    sys.exit(app.exec_())
